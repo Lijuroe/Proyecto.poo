2a5878ef569d31f39783b593a47586cb507d376b
Lijuroe <71603704+Lijuroe@users.noreply.github.com>
2020-09-20 12:13:50 -0500

    Update Cliente.java

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -1,5 +1,75 @@
-package model;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
 
+/**
+ *
+ * @author Julia
+ */
 public class Cliente {
+  private long codigoCliente;
+  private long cedula;
+  private String nombre;
+  private String Apellidos;
+  private String direccionEntrega;
+  private long telefono;
 
+    public Cliente() {
+    }
+    
+    public long getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(long codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public long getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(long cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    public String getDireccionEntrega() {
+        return direccionEntrega;
+    }
+
+    public void setDireccionEntrega(String direccionEntrega) {
+        this.direccionEntrega = direccionEntrega;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "codigoCliente=" + codigoCliente + ", cedula=" + cedula + ", nombre=" + nombre + ", Apellidos=" + Apellidos + ", direccionEntrega=" + direccionEntrega + ", telefono=" + telefono + '}';
+    }
 }
